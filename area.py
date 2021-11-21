085370dc5c308b093317069ada7e84138d7625db
MahaLakshmi0411 <94038154+MahaLakshmi0411@users.noreply.github.com>
2021-11-21 22:40:44 +0530

    The file prints area of circle
    And also gets the filename from the user and prints the extension of the file

--- area.py
+++ area.py
@@ -2,3 +2,6 @@ pi=3.14
 r=float(input("Enter the radius of a circle:"))
 area=pi*r*r
 print("The area of the circle is =%.2f"%area)
+i = input("Input the Filename: ")
+extns =i.split(".")
+print ("The extension of the file is : " + repr(extns[-1]))
