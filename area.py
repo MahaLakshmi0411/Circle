3311cfd5c4fc727c6ffa77c8bc906668ec8ab1c3
MahaLakshmi0411 <94038154+MahaLakshmi0411@users.noreply.github.com>
2021-11-22 08:21:40 +0530

    Update area.py

--- area.py
+++ area.py
@@ -4,4 +4,6 @@ area=pi*r*r
 print("The area of the circle is =%.2f"%area)
 i = input("Input the Filename: ")
 extns =i.split(".")
+
+# repr() function is used to returns a printable representation of a object(optional) 
 print ("The extension of the file is : " + repr(extns[-1]))
